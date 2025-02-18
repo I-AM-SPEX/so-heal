4655d2842a1fb8b7fac8a490cc0bacb1f1c74364
Samuel Williams <samuelspexy@gmail.com>
2025-02-18 02:40:23 +0200

    Nativewind setup

--- apps/mobile/app/index.tsx
+++ apps/mobile/app/index.tsx
@@ -9,7 +9,9 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text className="font-bold text-lg">
+        Easy does it Samuel . We gonna win this.
+      </Text>
     </View>
   );
 }
