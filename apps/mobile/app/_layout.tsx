24b4cf87817dfcf4e8c95d33f492caee7ac6848d
Samuel Williams <samuelspexy@gmail.com>
2025-02-21 20:46:32 +0200

    Added:Main app  Navigation layout

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -1,5 +1,16 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 import "./global.css";
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <>
+      <StatusBar style="auto" />
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="(auth)" />
+
+        <Stack.Screen name="(drawer)" options={{ gestureEnabled: false }} />
+        <Stack.Screen name="index" />
+      </Stack>
+    </>
+  );
 }
