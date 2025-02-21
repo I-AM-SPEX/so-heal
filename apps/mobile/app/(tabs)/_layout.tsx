dd31f42530925357fdb197f58a855294a5c9aee5
Samuel Williams <samuelspexy@gmail.com>
2025-02-21 06:46:52 +0200

    Refactored: Primary tabs layout

--- apps/mobile/app/(tabs)/_layout.tsx
+++ apps/mobile/app/(tabs)/_layout.tsx
@@ -3,26 +3,25 @@ import PrimaryTabs from "@/components/navigation/PrimaryTabs";
 const PrimaryTabsLayout = () => {
   return (
     <Tabs
-      initialRouteName="home/index"
       tabBar={(props) => <PrimaryTabs {...props} />}
-      backBehavior="history"
+      //backBehavior="history"
       screenOptions={{ headerShown: false }}
     >
       <Tabs.Screen
-        name="home/index"
-        options={{ href: "/(tabs)/home", animation: "fade" }}
+        name="feed"
+        options={{ href: "/feed/screens", animation: "fade" }}
       />
       <Tabs.Screen
-        name="connect/index"
-        options={{ href: "/(tabs)/connect", animation: "fade" }}
+        name="connections"
+        options={{ href: "/connections/screens", animation: "fade" }}
       />
       <Tabs.Screen
-        name="communities/index"
-        options={{ href: "/(tabs)/communities", animation: "fade" }}
+        name="community"
+        options={{ href: "/community/screens", animation: "fade" }}
       />
       <Tabs.Screen
-        name="wellness/index"
-        options={{ href: "/(tabs)/wellness", animation: "fade" }}
+        name="wellness"
+        options={{ href: "/wellness/screens", animation: "fade" }}
       />
     </Tabs>
   );
